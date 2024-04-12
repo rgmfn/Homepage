981faade94ebc2e8254d0d00154c41ceb9b62a04
Ryan Gragg <rygragg@gmail.com>
2024-04-12 11:04:14 -0700

    small footer first draft

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,35 @@
 import React from 'react'
 
-import Empty from './Empty';
+import './Footer.css'
+
+const Link = ({ href, children }) => (
+    <a
+        href={href}
+        target="_blank"
+        rel="noreferrer"
+        className="link"
+    >
+        {children}
+    </a>
+);
 
 /**
  *
  */
 function Footer({ footerRef }) {
     return (
-        <div ref={footerRef}>
-            <div>Footer</div>
-            <Empty />
+        <div className="footer-container" ref={footerRef}>
+            <hr className="seperator" />
+            <div className="contacts">
+                <div className="email">
+                    <Link href="mailto:rgmfn.dev@gmail.com">Email</Link>
+                    {/*<div>rgmfn.dev@gmail.com</div>*/}
+                </div>
+                <Link href="https://www.linkedin.com/in/rgmfn/">LinkedIn</Link>
+                <Link href="https://github.com/rgmfn">Github</Link>
+                <Link href="https://www.youtube.com/@RagamuffinAround">Youtube</Link>
+                <Link href="https://github.com/rgmfn/Homepage">Source Code</Link>
+            </div>
         </div>
     )
 }
