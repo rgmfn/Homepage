8a814cc719e5062fa06bfc48b59c20a7eb737494
Ryan Gragg <rygragg@gmail.com>
2024-04-15 15:25:26 -0700

    using vw/vh, removed some sections, add scroll

--- src/Splash.js
+++ src/Splash.js
@@ -21,9 +21,10 @@ const Section = ({ sectionRef, children }) => (
  * TODO
  */
 function Splash({
-    worksRef, aboutRef, videosRef, musicRef, footerRef
+    worksRef, aboutRef, videosRef, musicRef, footerRef, isSmallScreen
 }) {
-    gsap.registerPlugin(ScrollTrigger)
+    // setting up element references for gsap
+    gsap.registerPlugin(ScrollTrigger);
 
     const whiteRef = useRef();
     const topRef = useRef();
@@ -31,21 +32,22 @@ function Splash({
     const botRef = useRef();
     const sectionsRef = useRef();
     const descriptionRef = useRef();
-
-    // TODO: grow section font size when u hover? :)
+    const scrollRef = useRef();
 
     useGSAP(() => {
         let tl = gsap.timeline({ delay: 0.4 })
 
-        gsap.set([topRef.current, midRef.current, botRef.current], { opacity: 1, y: 0 });
+        gsap.set([
+            topRef.current, midRef.current, botRef.current, scrollRef.current,
+        ], { opacity: 1, y: 0 });
 
         // seperate three lines
         tl.from(topRef.current, {
             duration: 1.4, opacity: 0.8, ease: Power2.easeInOut,
         }).from(midRef.current, {
-            duration: 1.4, opacity: 0.8, y: -146, ease: Power2.easeInOut,
+            duration: 1.4, opacity: 0.8, y: "-11.78vw", ease: Power2.easeInOut,
         }, 0).from(botRef.current, {
-            duration: 1.4, opacity: 0.8, y: -294, ease: Power2.easeInOut,
+            duration: 1.4, opacity: 0.8, y: "-23.50vw", ease: Power2.easeInOut,
             // fade out white overlay text
         }, 0).from(whiteRef.current, {
             duration: 0.8, opacity: 1, ease: Power2.easeInOut,
@@ -61,32 +63,41 @@ function Splash({
             // fade in + move description
         }, "<").from(descriptionRef.current, {
             duration: 1.4, x: -50, opacity: 0, ease: Power2.easeInOut,
-        }, "<");
+        }, "<").from(scrollRef.current, {
+            duration: 1.4, y: -50, opacity: 0, ease: Power2.easeInOut,
+        });
     });
 
     return (
         <div className="splash-container">
             <div className="splash-top">
-                <div className="splash-left">
-                    <img id="logo" src="/images/icon.png" />
-                    <div className="title-container">
-                        <div className="white name" ref={whiteRef}>HI, I'M RYAN.</div>
-                        <div className="top name" ref={topRef}>HI, I'M RYAN.</div>
-                        <div className="middle name" ref={midRef}>HI, I'M RYAN.</div>
-                        <div className="bottom name" ref={botRef}>HI, I'M RYAN.</div>
-                    </div>
+                <div className="title-container">
+                    <div className="white" ref={whiteRef}>HI, I'M RYAN.</div>
+                    <div className="top" ref={topRef}>HI, I'M RYAN.</div>
+                    <div className="middle" ref={midRef}>HI, I'M RYAN.</div>
+                    <div className="bottom" ref={botRef}>HI, I'M RYAN.</div>
                 </div>
-                <div className="sections-container" ref={sectionsRef}>
-                    <Section sectionRef={worksRef}>Works</Section>
-                    <Section sectionRef={aboutRef}>About</Section>
-                    <Section sectionRef={videosRef}>Videos</Section>
-                    <Section sectionRef={musicRef}>Music</Section>
-                    <Section sectionRef={footerRef}>Contact</Section>
-                </div>
+                {
+                    !isSmallScreen && (
+                        <div className="sections-container" ref={sectionsRef}>
+                            <Section sectionRef={worksRef}>Works</Section>
+                            <Section sectionRef={aboutRef}>About</Section>
+                            <Section sectionRef={videosRef}>Videos</Section>
+                            {/* <Section center={true} sectionRef={musicRef}>Music</Section> */}
+                            {/* <Section sectionRef={footerRef}>Contact</Section> */}
+                        </div>
+                    )
+                }
             </div>
             <div className="description" ref={descriptionRef}>
                 Software developer always looking to learn, explore, and grow.<br /><br />
                 Looking for work!
+            </div>
+            <div
+                className="scroll"
+                ref={scrollRef}
+            >
+                scroll...
             </div>
         </div>
     )
