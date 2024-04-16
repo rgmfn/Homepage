37e4aa5c3acf96a984c7e7724d9ad99485b37f10
Ryan Gragg <rygragg@gmail.com>
2024-04-15 18:06:46 -0700

    linting

--- src/Works.js
+++ src/Works.js
@@ -1,7 +1,7 @@
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 
 import './Works.css'
 
